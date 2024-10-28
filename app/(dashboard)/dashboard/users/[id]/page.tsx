af81d358628c176f2e83c9f34e3abf31a31ebd0f
Farok Hossain <farokafs7@gmail.com>
2024-10-28 17:58:53 +0600

    next-auth setup

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-const Page = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  return <h1 className="text-center text-2xl">User Profile: {id}</h1>;
-};
-
-export default Page;
