9b9bbfc3533c007640c1cc46aa2afccb7f93a8fb
Farok Hossain <farokafs7@gmail.com>
2024-10-30 18:26:14 +0600

    components updated

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,5 +1,6 @@
 import Form from "next/form";
 import SearchFormReset from "./SearchFormReset";
+import { Search } from "lucide-react";
 
 const SearchForm = ({ query }: { query?: string }) => {
   return (
@@ -12,7 +13,9 @@ const SearchForm = ({ query }: { query?: string }) => {
       />
       <div className="flex gap-2">
         {query && <SearchFormReset />}
-        <button className="search-btn text-white">S</button>
+        <button className="search-btn text-white">
+          <Search className="size-5" />
+        </button>
       </div>
     </Form>
   );
