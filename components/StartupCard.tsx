6e9b01fc936b2ed15ba804d0450f68b47e85b347
Farok Hossain <farokafs7@gmail.com>
2024-11-01 22:10:32 +0600

    updated

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-const StartupCard = ({ post }: { post: StartupCardType }) => {
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
     _createdAt,
     views,
